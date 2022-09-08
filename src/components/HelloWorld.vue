fc9a145336bf8565ff8ca023d3dea9a1a9e3f750
xiaochong <897411890@qq.com>
2022-09-08 16:55:15 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .hello{
+    background: url(https://static.zhihu.com/heifetz/assets/sign_bg.db29b0fb.png) no-repeat;
+    width: 100%;
+    height: 100vh;
+  }
+</style>
